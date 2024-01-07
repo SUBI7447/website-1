77dc2bfb83a9d49d341180733f8115e5d959c232
Krisha <krishaaggarwal.mit@gmail.com>
2024-01-07 18:49:45 +0530

    rendering the model and the scene

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react';
+import { Canvas, useLoader} from '@react-three/fiber';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader';
+import { Environment, OrbitControls } from '@react-three/drei';
+import obj from './models/mlsc1.glb';
+import './App.css'
 
-function App() {
+const Model = () => {
+  const modelRef = useRef();
+
+  const gltf = useLoader(GLTFLoader, obj);
+
+  if (gltf && gltf.scene) {
+    modelRef.current = gltf.scene;
+    modelRef.current.scale.set(1, 1, 1);
+    modelRef.current.position.set(0, -10, -20);
+    modelRef.current.rotation.set(0, -Math.PI / 2, 0);
+  }
+
+  return <primitive object={modelRef.current} />;
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className='full-page-container'>
+    <Canvas>
+      <ambientLight />
+      <pointLight position={[10, 10, 10]} />
+
+      {/* Set up the environment texture using a cube map */}
+      <Environment preset="warehouse" />
+
+      {/* Adjust the camera position */}
+      <perspectiveCamera position={[0, 0, 5]} />
+
+      <Model />
+
+      {/* Add orbit controls for interaction (optional) */}
+      <OrbitControls />
+    </Canvas>
     </div>
   );
-}
+};
 
 export default App;
