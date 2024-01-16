54e04c2c821b0ca548a9c38394e6f87940d40c7b
Krisha <krishaaggarwal.mit@gmail.com>
2024-01-17 00:47:46 +0530

    responsive home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import './Home.css'
+import mlsa from '../assets/MLSA.png'
 
 const Home = () => {
   return (
     <div className='home'>
       <div className='home-content'>
         <div className='home-text'>
-            
+        <span className='mlsc-text'><h1>Microsoft Learn Student Club</h1></span>
+        <span className='mlsc-about-text'><h3>Accelerate innovation and grow the skills you need to have greater impact in the projects and communities that matter to you.</h3></span>
+        <button className='application'>Join The Club</button>
+        </div>
+        <div className='home-image'>
+        <img src={mlsa} alt="microsoft learn student ambassador logo"/>
         </div>
       </div>
     </div>
