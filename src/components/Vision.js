8d7246fe0e744fdae94a075e2284b6649e5ec04d
Krisha <krishaaggarwal.mit@gmail.com>
2024-01-17 23:31:56 +0530

    vision top text black

--- src/components/Vision.js
+++ src/components/Vision.js
@@ -10,11 +10,11 @@ const Vision = () => {
 
   return (
     <div className='vision'>
-      <span className='vision-header'>Microsoft Learn Student Club MIT ADT</span>
-      <div className='vision-line'>&#x2022; Build the Future &#x2022; &emsp; &emsp; &#x2022; Grow your community &#x2022; </div>
-        <div className='vision-ambassador-logo'>
-        <div className='vision-statement'></div>
-        <div className='vision-info'></div>
+      <span className='vision-header'>MLSC MIT ADT</span>
+      <div className='vision-line'>&#x2022; Build the Future &#x2022; &emsp; &emsp; &#x2022; Grow your community &#x2022; &emsp; &emsp; &#x2022; Become an Ambassador &#x2022;&emsp; &emsp; &#x2022; Fly on the cloud &#x2022;</div>
+        <div className='vision-ambassador'>
+        <div className='vision-statement'>Join Us and Become an MLSA</div>
+        <div className='vision-info'>Achieve more with unique opportunities to build AI-driven solutions, explore cutting-edge technology, lead local communities, and grow online presence.<br/>Develop professional skills that align with your interests and aspirations.<br/>Share ideas, solve challenges, and expand your circle of influence.</div>
       </div>
       <div className='vision-pic'></div>
     </div>
