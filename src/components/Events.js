4f42a82530900158a77ddbaff8c34420051700db
SUBI7447 <subodhsubi362@gmail.com>
2024-04-25 19:46:34 +0530

    ADDED POSTER , EDITED EVENT(DIV) , CHANGED IN ABOUT(DIV)

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,22 +1,23 @@
 import React from 'react'
-import './Home.css'
-import mlsa from '../assets/MLSA.png'
+import './Events.css'
+import day_3 from '../assets/day_3.jpg'
 
-const Home = () => {
+
+const Events = () => {
   return (
-    <div className='home'>
-      <div className='home-content'>
-        <div className='home-text'>
-        <span className='mlsc-text'><h1>Microsoft Learn Student Club</h1></span>
-        <span className='mlsc-about-text'><h3>Accelerate innovation and grow the skills you need to have greater impact in the projects and communities that matter to you.</h3></span>
-        <a className='application'href="https://docs.google.com/forms/d/e/1FAIpQLSe3C9Ir59imQB0CqxzPFmShW86uMe_mSVXH5G--57HyJFv21A/viewform?usp=sf_link" target="_blank">Join The Club</a>
+    <div className='event'>
+      <span className='title'>Events</span>
+      <div className='event-content'>
+
+        <div className='posters'>
+          <img src={day_3} alt="day_3" />
+
         </div>
-        <div className='home-image'>
-        <img src={mlsa} alt="microsoft learn student ambassador logo"/>
-        </div>
+        <a className="reg-button" href="https://docs.google.com/forms/d/e/1FAIpQLSdq2EoDe8JRW5uBniYG9OPC3lGcQ3mXszfkGhdZ32cYTbhQsg/viewform" target="_blank">Register now</a>
       </div>
     </div>
+
   )
 }
 
-export default Home
+export default Events
