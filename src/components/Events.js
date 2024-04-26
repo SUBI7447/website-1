746ba2b2d71a6b6484c8da6afcb1fb9c1a8e4d46
SUBI7447 <subodhsubi362@gmail.com>
2024-04-26 16:24:14 +0530

    FIXED A BUG IN EVENT(DIV)  (BUG :  NAV BAR GETS HIDE UNDER EVENT DIV)

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import './Events.css'
 import day_3 from '../assets/day_3.jpg'
+import day_2 from '../assets/day_2.jpg'
 
 
 const Events = () => {
   return (
     <div className='event'>
-      <span className='title'>Events</span>
+      <span className='title'>Event</span>
       <div className='event-content'>
 
-        <div className='posters'>
-          <img src={day_3} alt="day_3" />
 
+
+        <div className='poster'>
+          <span className='event-name'><h3>Day 3</h3></span>
+          <img src={day_3} alt="day 3" />
         </div>
         <a className="reg-button" href="https://docs.google.com/forms/d/e/1FAIpQLSdq2EoDe8JRW5uBniYG9OPC3lGcQ3mXszfkGhdZ32cYTbhQsg/viewform" target="_blank">Register now</a>
       </div>
     </div>
-
   )
 }
 
